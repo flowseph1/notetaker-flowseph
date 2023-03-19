249c7774dab918d48b3887d471c1557829ae00d5
flowseph1 <josephthenewandarmonius@gmail.com>
2023-03-19 10:13:43 -0600

    adding whole app

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { topiRouter } from "./routers/topic";
+import { noteRouter } from "./routers/note";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topic: topiRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
